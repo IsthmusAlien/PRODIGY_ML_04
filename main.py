e4ec2d66fa816c5000d571182cd0a6fc5dee2610
IsthmusAlien <131596732+IsthmusAlien@users.noreply.github.com>
2023-12-19 20:53:29 +0530

    Updated

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ import cv2
 import subprocess as sp
 import pyautogui
 
+url = "http://192.168.0.102:8080/video"
+
 BaseOptions = mp.tasks.BaseOptions
 GestureRecognizer = mp.tasks.vision.GestureRecognizer
 GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
@@ -10,9 +12,10 @@ GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
 VisionRunningMode = mp.tasks.vision.RunningMode
 
 
-video = cv2.VideoCapture(0)
-counter = {'stop': 0, 'two_up': 0, 'fist': 0}
-period = 60
+video = cv2.VideoCapture(url)
+counter1 = {'stop': 0, 'one': 0, 'fist': 0}
+counter2 = {'stop': True, 'one': True, 'fist': True}
+period = 5
 
 def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
     for gesture in result.gestures:
@@ -22,27 +25,35 @@ def print_result(result: GestureRecognizerResult, output_image: mp.Image, timest
 
 def work(pose):
     if pose == "stop":
-        counter['stop'] += 1
-        if counter['stop'] == period:
+        counter1['stop'] += 1
+        if counter1['stop'] == period and counter2['stop']:
+            print("open notepad")
             programName = "notepad.exe"
             fileName = "sample.txt"
             sp.Popen([programName, fileName])
-            counter['stop'] = 0
+            counter1['stop'] = 0
+            counter2['stop'] = False
 
     elif pose == "fist":
-        counter['fist'] += 1
-        if counter['fist'] == period:
+        counter1['fist'] += 1
+        if counter1['fist'] == period and counter2['fist']:
+            print("close notepad")
             sp.call("TASKKILL /F /IM notepad.exe", shell=True)
-            counter['fist'] = 0
+            counter1['fist'] = 0
+            counter2['fist'] = False
 
-    elif pose == "two_up":
-        counter['two_up'] += 1
-        if counter['two_up'] == period:
+    elif pose == "one":
+        counter1['one'] += 1
+        if counter1['one'] == period and counter2['one']:
+            print("Print Done")
             pyautogui.write('Done')
-            counter['two_up'] = 0
+            counter1['one'] = 0
+            counter2['one'] = False
 
 def show(frame):
-    cv2.imshow("FEED", frame)
+    cv2.namedWindow("output", cv2.WINDOW_NORMAL)
+    cv2.resizeWindow("output", 600, 600)
+    cv2.imshow("output", frame)
 
 options = GestureRecognizerOptions(
     base_options=BaseOptions(model_asset_path='model4.task'),
